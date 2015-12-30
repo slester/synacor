33fd29e6eaff5218be8b6c5423a7c9769adde02d
Stephen Lester <stephen@lester.io>
2015-12-30 13:58:41 -0800

    abandoning, still failing on 890

--- machine/src/machine/core.clj
+++ machine/src/machine/core.clj
@@ -1,26 +1,27 @@
 (ns machine.core
   (:require [clojure.java.io :as io]
             [gloss.core :as g]
-            [gloss.io :as gio]
-            )
-  (:import [org.apache.commons.io IOUtils])
+            [gloss.io :as gio])
+  (:import [org.apache.commons.io IOUtils]))
 
-  (:gen-class))
-
+(def first-register 32768)
 (def registers (vec (repeat 8 0)))
 (def stack '())
 
 (g/defcodec i16 :uint16-le)
+(defn read-file [file-path] (IOUtils/toByteArray (io/input-stream file-path)))
 
-(defn read-file [file-path]
-  (IOUtils/toByteArray (io/input-stream file-path)))
-
-(def first-register 32768)
 (defn rkey [k] (- (int k) first-register))
 (defn rval [rs k] (rs (rkey k)))
 (defn resolve-val [rs bs v] (println "resolving" v)
+  ;; not in a register
   (if (> first-register v)
-    (if (> first-register (bs v)) (bs v) (resolve-val rs bs (rval rs (bs v))))
+    ;; value is not a register
+    (if (> first-register (bs v))
+      (bs v)
+      ;; value is in a register, so it needs resolving
+      (resolve-val rs bs (bs v)))
+    ;; value is in a register, so get its value
     (resolve-val rs bs (rval rs v))))
 (defn val-of [rs v] (if (> first-register v) v (rval rs v)))
 ;; (defn addr-val [rs bs v] (if (> first-register v) (bs v) (bs (rval rs v))))
@@ -35,87 +36,89 @@
     ;; set: 1 a b - set register <a> to the value of <b>
     1 (let [a (mem (inc pos))
             b (mem (+ 2 pos))]
-        ;; (println "SET: register" (rkey a) "to" (val-of rs b))
+        (println "SET: register" (rkey a) "to" (val-of rs b))
         [(+ 3 pos) mem (assoc rs (rkey a) (val-of rs b)) s])
     ;; push: 2 a - push <a> onto the stack
     2 (let [a (mem (inc pos))
             v (val-of rs a)]
+        (println "PUSH" a "onto the stack")
         [(+ 2 pos) mem rs (conj s v)])
     ;; pop: 3 a - remove the top element from the stack and write it into <a>; empty stack = error
     3 (let [a (mem (inc pos))
             r (rkey a)
             v (peek s)]
+        (println "POP and write to" a)
         [(+ 2 pos) mem (assoc rs r v) (pop s)])
     ;; eq: 4 a b c - set <a> to 1 if <b> is equal to <c>; set it to 0 otherwise
     4 (let [a (mem (inc pos))
             b (mem (+ 2 pos))
             c (mem (+ 3 pos))]
         (println "EQ?" (val-of rs b) (val-of rs c) "to store in" (rkey a))
+        (println "EQ:" a b c)
         [(+ 4 pos) mem (assoc rs (rkey (mem (inc pos)))
                               (if (= (val-of rs b) (val-of rs c)) 1 0)) s])
     ;; gt: 5 a b c - set <a> to 1 if <b> is greater than <c>; set it to 0 otherwise
     5 (let [a (mem (inc pos))
             b (mem (+ 2 pos))
             c (mem (+ 3 pos))]
-        ;; (println "GT?" (val-of rs b) (val-of rs c) "to store in" (rkey a))
+        (println "GT?" (val-of rs b) (val-of rs c) "to store in" (rkey a))
         [(+ 4 pos) mem (assoc rs (rkey a)
                               (if (> (val-of rs b) (val-of rs c)) 1 0)) s])
     ;; jmp: 6 a - jump to <a>
     6 (let [a (mem (inc pos))]
-        ;; (println "JUMPING TO" (int a))
+        (println "JUMPING TO" (int a))
         [(int a) mem rs s])
     ;; jt: 7 a b -  if <a> is nonzero, jump to <b>
     7 (let [a (mem (inc pos))
             b (mem (+ 2 pos))]
-        ;; (println (mem (inc pos)))
-        ;; (println "JUMPING TO" (val-of rs b) "IF" (val-of rs a) "=/= 0")
+        (println "JUMPING TO" (val-of rs b) "IF" (val-of rs a) "=/= 0")
         [(if (not= 0 (val-of rs a)) (int (val-of rs b)) (+ 3 pos)) mem rs s])
     ;; jf: 8 a b - if <a> is zero, jump to <b>
     8 (let [a (mem (inc pos))
             b (mem (+ 2 pos))]
-        ;; (println (mem (inc pos)))
-        ;; (println "JUMPING TO" (val-of rs b) "IF" (val-of rs a) "= 0")
+        (println "JUMPING TO" (val-of rs b) "IF" (val-of rs a) "= 0")
         [(if (= 0 (val-of rs a)) (int (val-of rs b)) (+ 3 pos)) mem rs s])
     ;; add: 9 a b c - assign into <a> the sum of <b> and <c> (modulo 32768)
     9 (let [a (mem (inc pos))
             b (mem (+ 2 pos))
             c (mem (+ 3 pos))]
-        ;; (println "ADDING" (val-of rs b) (val-of rs c) "to store in" (rkey a))
+        (println "ADDING" (val-of rs b) (val-of rs c) "to store in" (rkey a))
         [(+ 4 pos) mem (assoc rs (rkey a)
                               (mod (+ (val-of rs b) (val-of rs c)) 32768)) s])
     ;; mult: 10 a b c - store into <a> the product of <b> and <c> (modulo 32768)
     10 (let [a (mem (inc pos))
              b (mem (+ 2 pos))
              c (mem (+ 3 pos))]
-         ;; (println "MULT" (val-of rs b) (val-of rs c) "to store in" (rkey a))
+         (println "MULT" (val-of rs b) (val-of rs c) "to store in" (rkey a))
          [(+ 4 pos) mem (assoc rs (rkey a)
                                (mod (* (val-of rs b) (val-of rs c)) 32768)) s])
     ;; mod: 11 a b c - store into <a> the remainder of <b> divided by <c>
     11 (let [a (mem (inc pos))
              b (mem (+ 2 pos))
              c (mem (+ 3 pos))]
-         ;; (println "MOD" (val-of rs b) (val-of rs c) "to store in" (rkey a))
+         (println "MOD" (val-of rs b) (val-of rs c) "to store in" (rkey a))
          [(+ 4 pos) mem (assoc rs (rkey a)
                                (rem (val-of rs b) (val-of rs c))) s])
     ;; and: 12 a b c - stores into <a> the bitwise and of <b> and <c>
     12 (let [a (mem (inc pos))
              b (mem (+ 2 pos))
              c (mem (+ 3 pos))]
-         ;; (println "AND" (val-of rs b) (val-of rs c) "to store in" (rkey a))
+         (println "AND" (val-of rs b) (val-of rs c) "to store in" (rkey a))
          [(+ 4 pos) mem (assoc rs (rkey a)
                                (bit-and (val-of rs b) (val-of rs c))) s])
     ;; or: 13 a b c - stores into <a> the bitwise or of <b> and <c>
     13 (let [a (mem (inc pos))
              b (mem (+ 2 pos))
              c (mem (+ 3 pos))]
-         ;; (println "OR" (val-of rs b) (val-of rs c) "to store in" (rkey a))
+         (println "OR" (val-of rs b) (val-of rs c) "to store in" (rkey a))
          [(+ 4 pos) mem (assoc rs (rkey a)
                                (bit-or (val-of rs b) (val-of rs c))) s])
     ;; not: 14 a b - stores 15-bit bitwise inverse of <b> in <a>
     14 (let [a (mem (inc pos))
              b (mem (+ 2 pos))]
-         ;; (println "NOT" (val-of rs b))
+         (println "NOT" (val-of rs b))
          [(+ 3 pos) mem (assoc rs (rkey a)
+                               ;; discard the MSB
                                (bit-and 2r0111111111111111 (bit-not (val-of rs b)))) s])
     ;; rmem: 15 a b - read memory at address <b> and write it to <a>
     15 (let [a (mem (inc pos))
@@ -128,19 +131,21 @@
     16 (let [a (mem (inc pos))
              b (mem (+ 2 pos))
              a-val (rval rs a)]
-         (println "WMEM value from" b "into memory at address" a-val)
+         (println "WMEM value from" b "into memory at address" a)
          (println b "->" a-val)
          [(+ 3 pos)
           (if (>= a-val first-register) mem (assoc mem (int a-val) b))
-          (if (>= a-val first-register) (assoc rs (rkey a-val) b) rs) s])
+          (if (>= a-val first-register) (assoc rs (rkey a-val) b) rs)
+          s])
     ;; call: 17 a - write the address of the next instruction to the stack and jump to <a>
     17 (let [a (mem (inc pos))]
-         ;; (println "CALL adding to the stack:" (+ 2 pos) "then jumping to" (val-of rs a))
+         (println "CALL adding to the stack:" (+ 2 pos) "then jumping to" (val-of rs a))
          [(int (val-of rs a)) mem rs (conj s (+ 2 pos))])
     ;; ret: 18 - remove the top element from the stack and jump to it; empty stack = halt
     18 (let [n (peek s)]
+         (println "RET: JUMP TO" n)
          (if (nil? n)
-           [-1 rs s]
+           [-1 mem rs s]
            [(int n) mem rs (pop s)]))
     ;; out: 19 a - write the character represented by ascii code <a> to the terminal
     19 (let [c (char (val-of rs (mem (inc pos))))]
